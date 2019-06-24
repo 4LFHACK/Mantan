f3d6b867d15da8642b876fd4d264c97ac19031b5
4LFHACK <51878221+4LFHACK@users.noreply.github.com>
2019-06-24 02:53:24 +0000

    Add files via upload

---
+++ Mantan.sh
@@ -0,0 +1,59 @@
+#hanya prank gan
+#voucernya sudah pernah di gunakan
+#clear
+figlet Ciee Jones | lolcat
+echo
+echo " crack voucher pulsa 3 " | lolcat
+echo
+echo " cara mengaktifkan voucher pulsa 3 *111*(16 angka kode voucher) yes or no"
+echo
+echo " jawaban [y or n] " | lolcat
+echo
+read alphabet
+if [ $alphabet = y ] || [ $alphabet = yes ]
+then
+echo "Load"
+sleep 2
+echo " 10% " | lolcat
+sleep 1
+echo " 20% " | lolcat
+sleep 2
+echo " 30% " | lolcat
+sleep 2
+echo " 40% " | lolcat
+sleep 3
+echo " 50% " | lolcat
+sleep 2
+echo " 60% " | lolcat
+sleep 5
+echo " 70% " | lolcat
+echo " 80% " | lolcat
+echo " 90% " | lolcat
+sleep 5
+echo " 100% " | lolcat
+sleep 4
+echo " succes"
+sleep 1
+
+echo
+echo
+echo  "berikut ini adalah voucher yang berhasil di dapatkan" | lolcat
+sleep 5
+echo
+echo
+echo " 8816372105624438 " | lolcat
+echo
+echo
+echo "gunakan tools ini dengan benar" | lolcat
+echo "semoga harimu bahagia" | lolcat
+echo "terima kasih sudah mampir                                      by : ALFHACK " | lolcat
+fi
+
+if [ $alphabet = n ] || [ $alphabet = no ]
+then
+echo "kasihan"
+echo
+echo " pencarian tidak di temukan 😭 "
+echo
+echo " thanks sudah mampir                                            by : ALFHACK" | lolcat
+fi
